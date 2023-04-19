089bd61689a545a4df7854f99cb48f3721662aac
sumeet.manit@gmail.com <sumeet.manit@gmail.com>
2023-04-19 14:00:39 +0000

    ❇️ New     - Job        - apr19

---
+++ jobs/apr19/code/dag.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pendulum
+from datetime import timedelta
+import airflow
+from airflow.decorators import dag
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from tasks import 
+PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
+
+@dag(
+     dag_id = "sumit_sumit_dbt_apr19", 
+     schedule_interval = "0 0/1 * * *", 
+     default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
+     start_date = airflow.utils.dates.days_ago(0), 
+     catchup = True
+)
+def dag():
+    pass
+
+dag()
